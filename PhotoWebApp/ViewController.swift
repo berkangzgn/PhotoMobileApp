b0e59bebde7a6693dc4a52aed9a6686b445feca6
berkangzgn <bgezgin@protonmail.com>
2021-11-12 14:17:06 +0300

    Adding user interface

--- PhotoWebApp/ViewController.swift
+++ PhotoWebApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func signinClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
 }
 
