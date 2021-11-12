4fd4bcc810a1f3ca19420b10cd15150ee8955fb9
berkangzgn <bgezgin@protonmail.com>
2021-11-12 15:22:21 +0300

    User registration added

--- PhotoWebApp/ViewController.swift
+++ PhotoWebApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -20,12 +21,33 @@ class ViewController: UIViewController {
     
     
     @IBAction func signinClicked(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            // Completion : İşlem bitince ne yapsın?
+            // Bu işlem asenkron olmalı ki kullanıcı kaydı sırasında kullanıcıyı arayüzde kilitlemeyelim
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { AuthDataResult, Error in
+                if Error != nil {
+                    self.errorMessage(titleInput: "Error", messageInput: Error?.localizedDescription ?? "Beklenmedik bir hata oluştu.")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        } else {
+            errorMessage(titleInput: "Hatalı Giriş!", messageInput: "E-mail ya da parolanız boş bırakılamaz..")
+        }
     }
     
     
     @IBAction func loginClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
     }
     
+    
+    func errorMessage(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
