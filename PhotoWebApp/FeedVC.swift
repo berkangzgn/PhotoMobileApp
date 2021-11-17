c5e60a1259afbbfa08a5b5a5795c8a336f8e824a
berkangzgn <bgezgin@protonmail.com>
2021-11-17 16:38:02 +0300

    Refactoring

--- PhotoWebApp/FeedVC.swift
+++ PhotoWebApp/FeedVC.swift
@@ -13,9 +13,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+     /*
     var userArray = [String]()
     var commentArray = [String]()
-    var imageArray = [String]()
+    var imageArray = [String]() */
+    
+    var postArray = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +31,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func fbGetData() {
         let firestoreDB = Firestore.firestore()
-            // Altta verdiğimiz collection ismi kesinlikle DB'deki collection ismiyle uyuşmalı yoksa çalışmaz            ****************** Kontrol et çalışmayabilir *****************
-
+            // Altta verdiğimiz collection ismi kesinlikle DB'deki collection ismiyle uyuşmalı yoksa çalışmaz
             // Descending true : En yeni en üstte, false : En eski en üstte
         firestoreDB.collection("Post").order(by: "date", descending: true).addSnapshotListener { snapshot, error in
             if error != nil {
@@ -38,23 +40,26 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                     // Snapshot optional (Snapshot? : İçinde veri olabilir olmayabilir) olduğu için bunun kontrolünü yapmamız gerekiyor.
                 if snapshot?.isEmpty != true && snapshot != nil {
                     
-                        // Eskilerle birlikte kendini tekrarlamasın diye
+                    // Eskilerle birlikte kendini tekrarlamasın diye
+                    
+                    /*
                     self.userArray.removeAll(keepingCapacity: false)
                     self.commentArray.removeAll(keepingCapacity: false)
-                    self.imageArray.removeAll(keepingCapacity: false)
+                    self.imageArray.removeAll(keepingCapacity: false) */
+                    
+                    self.postArray.removeAll(keepingCapacity: false)
                     
                         // Documents bize verileri dizi halinde verir.
                     for document in snapshot!.documents {
                         // let documentID = document.documentID
                         
                         if let image = document.get("imageUrl") as? String {
-                            self.imageArray.append(image)
-                        }
-                        if let comment = document.get("commentText") as? String {
-                            self.commentArray.append(comment)
-                        }
-                        if let email = document.get("email") as? String {
-                            self.userArray.append(email)
+                            if let comment = document.get("commentText") as? String {
+                                if let email = document.get("email") as? String {
+                                    let post = Post(email: email, comment: comment, image: image)
+                                    self.postArray.append(post)
+                                }
+                            }
                         }
                     }
                     self.tableView.reloadData()
@@ -64,14 +69,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return userArray.count
+        return postArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.userTextField.text = userArray[indexPath.row]
-        cell.commentTextField.text = commentArray[indexPath.row]
-        cell.postImageView.sd_setImage(with: URL(string: self.imageArray[indexPath.row]), completed: nil)
+        cell.userTextField.text = postArray[indexPath.row].email
+        cell.commentTextField.text = postArray[indexPath.row].comment
+        cell.postImageView.sd_setImage(with: URL(string: self.postArray[indexPath.row].image), completed: nil)
         
         return cell
     }
