6fc5475fe16c6af25b5e0fd75918900aa0d50236
berkangzgn <bgezgin@protonmail.com>
2021-11-17 14:07:58 +0300

    Show data

--- PhotoWebApp/FeedVC.swift
+++ PhotoWebApp/FeedVC.swift
@@ -6,27 +6,63 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var userArray = [String]()
+    var commentArray = [String]()
+    var imageArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        fbGetData()
+    }
+    
+    func fbGetData() {
+        let firestoreDB = Firestore.firestore()
+            // Altta verdiğimiz collection ismi kesinlikle DB'deki collection ismiyle uyuşmalı yoksa çalışmaz            ****************** Kontrol et çalışmayabilir *****************
+
+        firestoreDB.collection("Post").addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                    // Snapshot optional (Snapshot? : İçinde veri olabilir olmayabilir) olduğu için bunun kontrolünü yapmamız gerekiyor.
+                if snapshot?.isEmpty != true && snapshot != nil {
+                        // Documents bize verileri dizi halinde verir.
+                    for document in snapshot!.documents {
+                        // let documentID = document.documentID
+                        
+                        if let image = document.get("imageUrl") as? String {
+                            self.imageArray.append(image)
+                        }
+                        if let comment = document.get("commentText") as? String {
+                            self.commentArray.append(comment)
+                        }
+                        if let email = document.get("email") as? String {
+                            self.userArray.append(email)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.userTextField.text = "berkan@gmail.com"
-        cell.commentTextField.text = "try"
+        cell.userTextField.text = userArray[indexPath.row]
+        cell.commentTextField.text = commentArray[indexPath.row]
         cell.postImageView.image = UIImage(named: "upload")
         
         return cell
